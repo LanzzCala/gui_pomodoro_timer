7be2206885da7943006af73de1b8dc543d839d5d
Lanzz Cala <lanzzcala@gmail.com>
2025-04-23 22:02:18 -0700

    Establish button feature of timer to receive input and count

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 import sys
 from PyQt6.QtCore import QSize
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout
 
 # Have QMainWindow be subclass to customize the window
 class PomodoroWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
         self.setWindowTitle("🍅Your Pomodoro Timer! 🍅")
-
-        self.number_times_clicked = 0
-
-        self.button = QPushButton("Pomodoro Time 🍅")
+        #Establish starting click amount
+        self.clicks = 0
+        self.button = QPushButton("Pomodoro Time 🍅", self)
+        self.button.setFixedSize(180, 120)
         self.button.clicked.connect(self.pomodoro_button_clicked)
-        self.setCentralWidget(self.button)
 
-        
-
+    
         self.setMinimumSize(QSize(400, 200))
         self.setMaximumSize(QSize(900, 600))
+        layout = QVBoxLayout()
+        layout.addWidget(self.button)
 
+    #Connect to parent and count each click of button
     def pomodoro_button_clicked(self):
-        count = self.number_times_clicked
-        new_count = count + 1
-        print(f"Number of clicks: {new_count}")
+        self.clicks += 1
+        self.button.setText(f"Pomodoros Clicked 🍅: {self.clicks}")
 
-
-application = QApplication(sys.argv)
-
-window = PomodoroWindow()
-window.show()
-application.exec()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = PomodoroWindow()
+    window.show()
+    sys.exit(app.exec())
