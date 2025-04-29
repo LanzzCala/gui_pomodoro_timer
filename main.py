494710706602e18e3819900266d946b3f783e013
Lanzz Cala <lanzzcala@gmail.com>
2025-04-28 17:38:10 -0700

    Encapsulate all code into single class

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from tkinter import *
 from tkinter import ttk
 
+class Pomodoro:
+    def __init__(self, root):
+
+
+        root.title("Pomodoro Timer")
+
+        mainframe = ttk.Frame(root, padding="3 3 12 12")
+        mainframe.grid(column=0, row=0, sticky= (N, W, E, S))
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+        ttk.Button(root, text="Pomodoro").grid()
+
+
 root = Tk()
-ttk.Button(root, text="Hello World").grid()
+Pomodoro(root)
 root.mainloop()
