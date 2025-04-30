f88de1c8c5143b6d39226fcc7eeb14ad52b54bfc
Lanzz Cala <lanzzcala@gmail.com>
2025-04-29 21:51:17 -0700

    Refactor code to have pause feature. Remove time.sleep for countdown, running, and pause flags

--- main.py
+++ main.py
@@ -1,93 +1,96 @@
 from tkinter import *
-from tkinter import ttk
-from tkinter import messagebox
-import time
+from tkinter import ttk, messagebox
 
 class Pomodoro:
     def __init__(self, root):
         self.root = root
-        root.title("Pomodoro Timer")
+        self.root.title("Pomodoro Timer")
 
         mainframe = ttk.Frame(root, padding="3 3 12 12")
-        mainframe.grid(column=0, row=0, sticky= (N, W, E, S))
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
         mainframe.pack(padx=10, pady=65)
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
         root.minsize(400, 200)
         root.maxsize(400, 200)
-        
-        #Establish primary buttons
+
+        # State tracking
+        self.running = False
+        self.paused = False
+        self.remaining_seconds = 0
+
+        # Buttons
         ttk.Button(mainframe, text="Pomodoro", command=self.pomodoro).grid(column=1, row=1, sticky=(N, W))
         ttk.Button(mainframe, text="Short break", command=self.short_break).grid(column=2, row=1, sticky=(N))
         ttk.Button(mainframe, text="Long break", command=self.long_break).grid(column=3, row=1, sticky=(S, W))
         ttk.Button(mainframe, text="Start", command=self.start).grid(column=1, row=3, sticky=(S, E))
         ttk.Button(mainframe, text="Reset", command=self.reset).grid(column=3, row=3, sticky=(S, W))
+        ttk.Button(mainframe, text="Pause", command=self.pause).grid(column=2, row=3)
 
-        #Declaration of variables
-        self.hour = StringVar()
-        self.minute = StringVar()
-        self.second = StringVar()
+        # Time entry fields
+        self.hour = StringVar(value="00")
+        self.minute = StringVar(value="00")
+        self.second = StringVar(value="00")
 
-        #Set default value of numbers to 0
-        self.hour.set("00")
-        self.minute.set("00")
-        self.second.set("00")
-        hour_entry = ttk.Entry(mainframe, width=6, font=(15), textvariable=self.hour)
-        hour_entry.grid(column=1, row=2)
-        minute_entry = ttk.Entry(mainframe, width=6, font=(15), textvariable=self.minute)
-        minute_entry.grid(column=2, row=2)
-        second_entry = ttk.Entry(mainframe, width=6, font=(15), textvariable=self.second)
-        second_entry.grid(column=3, row=2)
-        
+        ttk.Entry(mainframe, width=6, font=(15), textvariable=self.hour).grid(column=1, row=2)
+        ttk.Entry(mainframe, width=6, font=(15), textvariable=self.minute).grid(column=2, row=2)
+        ttk.Entry(mainframe, width=6, font=(15), textvariable=self.second).grid(column=3, row=2)
+
     def start(self):
-        try:
-            #Input provided by user is stored in temp
-            temp = int(self.hour.get())*3600 + int(self.minute.get())*60 + int(self.second.get())
-        except:
-            print("Please input the correct value")
-        while temp >-1:
-            # divmod(firstvalue = temp//60, secondvalue = temp%60)
-            mins,secs = divmod(temp,60)
+        if not self.running:
+            try:
+                self.remaining_seconds = int(self.hour.get()) * 3600 + int(self.minute.get()) * 60 + int(self.second.get())
+                self.running = True
+                self.paused = False
+                self.countdown()
+            except ValueError:
+                messagebox.showerror("Invalid Input", "Please enter valid numbers for the timer.")
 
-            # Logic to convert input entered in mins or secs to hours,
-            # mins, secs (input = 110 min --> 120 * 60 = 6600 => 1 Hr : 50 # Min: 0 sec)
-            hours = 0
-            if mins > 60:
+    def countdown(self):
+        if self.running and not self.paused:
+            if self.remaining_seconds <= 0:
+                messagebox.showinfo("Time Countdown", "Time's up!")
+                self.running = False
+                return
 
-                # divmod (firstvalue = temp//60, secondvalue = temp%60)
-                hours, mins = divmod(mins, 60)
+            mins, secs = divmod(self.remaining_seconds, 60)
+            hours, mins = divmod(mins, 60)
 
-            # using format () method to store the value up to two decimal places
-            self.hour.set("{0:2d}".format(hours))
-            self.minute.set("{0:2d}".format(mins))
-            self.second.set("{0:2d}".format(secs))
+            self.hour.set(f"{hours:02}")
+            self.minute.set(f"{mins:02}")
+            self.second.set(f"{secs:02}")
 
-            # Update GUI window after decrementing the temp value every time
-            self.root.update()
-            time.sleep(1)
+            self.remaining_seconds -= 1
+            self.root.after(1000, self.countdown)
 
-            # when temp value = 0; messagebox pops up message "time's up"
-            if (temp == 0):
-                messagebox.showinfo("Time Countdown", "Time's up ")
-
-            #after every one sec, value of temp will decrement
-            temp -= 1
-
-    def pomodoro(self):
-        self.minute.set("25")
-
-    def short_break(self):
-        self.minute.set("05")
-
-    def long_break(self):
-        self.minute.set("15")
+    def pause(self):
+        if self.running:
+            self.paused = not self.paused
+            if not self.paused:
+                self.countdown()  # Resume
 
     def reset(self):
+        self.running = False
+        self.paused = False
+        self.remaining_seconds = 0
         self.hour.set("00")
         self.minute.set("00")
         self.second.set("00")
-        
-        
+
+    def pomodoro(self):
+        self.hour.set("00")
+        self.minute.set("25")
+        self.second.set("00")
+
+    def short_break(self):
+        self.hour.set("00")
+        self.minute.set("05")
+        self.second.set("00")
+
+    def long_break(self):
+        self.hour.set("00")
+        self.minute.set("15")
+        self.second.set("00")
 
 
 root = Tk()
